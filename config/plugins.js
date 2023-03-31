c86e14a0006bec1257188216d1c73902dc77a5dc
Muditya Raghav <rmuditya@gmail.com>
2023-03-31 11:14:38 +0530

    post updated

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+
+module.exports = () => ({
+    //...
+    meilisearch: {
+        config: {
+            // Your meili host
+            host: "https://ms-8e25296c59d2-2973.sfo.meilisearch.io",
+            // Your master key or private key
+            api_key: "b0aa33f68a8f76b61cba10c6664e4dbfc93c1091",
+        }
+    }
+})
--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+
+    async create(ctx) {
+        const postman = ctx.request.body;
+        console.log(postman);
+
+
+    }
+
+
+
+}));
